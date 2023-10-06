8f67272f5451ab58d924581fc8d28af27e190fec
Cacildovaldo <123966041+Cacildovaldo@users.noreply.github.com>
2023-10-06 05:22:44 -0300

    AutomatedVaultManager.sol

--- AutomatedVaultManager.sol
+++ AutomatedVaultManager.sol
@@ -348,7 +348,7 @@ contract AutomatedVaultManager is Initializable, Ownable2StepUpgradeable, Reentr
     _results =
       IExecutor(_cachedVaultInfo.executor).onWithdraw(_cachedVaultInfo.worker, _vaultToken, _actualWithdrawAmount);
 
-    EXECUTOR_IN_SCOPE = address(0);
+    
     ///////////////////////////
     // Executor scope closed //
     ///////////////////////////
